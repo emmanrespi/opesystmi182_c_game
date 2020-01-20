9afb059a20cfc9bcb203fae371fcb8b006851bad
emmanrespi <ecrespicio@student.apc.edu.ph>
2020-01-20 02:50:47 +0000

    Final C Programming

--- jackenpoy.c
+++ jackenpoy.c
@@ -3,122 +3,143 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
-{
-    int Rock();
-    int Paper();
-    int Scissors();
-    int i;
-    int Pscore =0;
-    int Cscore =0;
-    int choice;
-    srand(time(NULL));
-    printf("Rock=1 , Paper= 2, and Scissors= 3\n");
+int Pscore =0;
+int Cscore =0;
+int choice;
 
+int  rock () {
+  printf("    ***      \n");;
+  printf("   *****     \n");;
+  printf("  *******    \n");;
+  printf("   *****     \n");;
+  printf("    ***      \n");;
+  printf("     *        \n");;
+}
+
+int paper(){
+  printf("      ******   \n");;
+  printf("     ******    \n");;
+  printf("    ******     \n");;
+  printf("   ******      \n");;
+  printf("  ******       \n");;
+  printf(" ******        \n");;
+}
+
+int scissors(){
+  printf("  **    **   \n");;
+  printf("   **  **    \n");;
+  printf("    ****     \n");;
+  printf("    ***      \n");;
+  printf("  **   **    \n");;
+  printf("  **   **    \n");;
+}
 
 int reload()
 {
-refresh();
+    refresh();
 
-int Rock (){
-  mvaddstr(10, 33, "    ***      ");
-  mvaddstr(10, 33, "   *****     ");
-  mvaddstr(11, 33, "   ******    ");
-  mvaddstr(12, 33, "   *****     ");
-  mvaddstr(13, 33, "    ***      ");
-  mvaddstr(13, 33, "     *       ");
-}
+    for(int i = 0; i < 5; i++) {
 
-int Paper (){
-  mvaddstr(10, 33, "    ******   ");
-  mvaddstr(10, 33, "   ******    ");
-  mvaddstr(11, 33, "   *****     ");
-  mvaddstr(12, 33, "  *****      ");
-  mvaddstr(13, 33, " *****       ");
-  mvaddstr(13, 33, " ****        ");
-}
-
-int Scissors (){
-  mvaddstr(10, 33, "   **   **   ");
-  mvaddstr(10, 33, "    ** **    ");
-  mvaddstr(11, 33, "     ***     ");
-  mvaddstr(12, 33, "     ***     ");
-  mvaddstr(13, 33, "    ** **    ");
-  mvaddstr(13, 33, "   **   **   ");
-}
-    for(i=0;i<5;i++){
         printf("Enter your choice:");
-        scanf("%d",&choice);
-            int computer=rand()%3+1;
-            if(choice==1){
-		Rock(); 
-                if(computer==1){
-			Rock();
-                    printf("Draw\n");
-                }
-                if(computer==2){
-			Paper();
-                    printf("Computer Wins!\n");
-                    Cscore= Cscore + 1;
-                }
-                if (computer==3){
-			Scissors();
-                    printf("Player Wins\n");
-                    Pscore = Pscore + 1;
-                }
-            }
-              else  if(choice==2){
-			Paper();
-                    if(computer==2){
-			Paper();
-                        printf("Draw\n");
-                    }
-                    if(computer==3){
-			Scissors();
-                        printf("Player Wins!\n");
-                        Pscore = Pscore + 1;
-                    }
-                    if(computer==1){  
-			Rock();
-                     printf("Computer Wins!\n");
-                        Cscore= Cscore + 1;
-                    }
-              }
-                else if(choice==3){
-			Scissors();
-                    if(computer==3){
-			Scissors();
-                        printf("Draw\n");
-                    }
-                    if(computer==2){
-			Paper();
-                        printf("Computer Wins!\n");
-                        Pscore= Pscore + 1;
-                    }
-                    if(computer==1){
-			Rock();
-                        printf("Player Wins!\n");
-                        Pscore = Pscore + 1;
+        scanf("%d", &choice);
 
-                    }
-                }
-        else{
+        int computer = rand() % 3 + 1;
+
+        if(choice==1){
+
+            printf("Player: Rock\n");
+            rock();
+
+            if(computer==1){
+                printf("\nComputer: Rock\n");
+                rock();
+                printf("\nDraw\n");
+            }
+            
+            if(computer==2){
+                printf("\nComputer: Paper\n");
+                paper();
+                printf("\nComputer Wins!\n");
+                Cscore= Cscore + 1;
+            }
+
+            if (computer==3){
+                printf("\nComputer: Scissors\n");
+                scissors();
+                printf("\nPlayer Wins\n");
+                Pscore = Pscore + 1;
+            }
+        } else  if(choice==2) {
+
+            printf("Player: Paper\n");
+            paper();
+
+            if(computer==2){
+                printf("\nComputer: Paper\n");
+                paper();
+                printf("\nDraw\n");
+            }
+
+            if(computer==3){
+                printf("\nComputer: Scissors\n");
+                scissors();
+                printf("\nPlayer Wins!\n");
+                Pscore = Pscore + 1;
+            }
+
+            if(computer==1){
+                printf("\nComputer: Rock\n");
+                rock();
+                printf("\nComputer Wins!\n");
+                Cscore= Cscore + 1;
+            }
+
+        } else if(choice==3){
+
+            printf("Player: Scissors\n");
+            scissors();
+
+            if(computer==3){
+                printf("\nComputer: Scissors\n");
+                scissors();
+                printf("\nDraw\n");
+            }
+            if(computer==2){
+                printf("\nComputer: Paper\n");
+                paper();
+                printf("\nComputer Wins!\n");
+                Pscore= Pscore + 1;
+            }
+            if(computer==1){
+                printf("\nComputer: Rock\n");
+                rock();
+                printf("\nPlayer Wins!\n");
+                Pscore = Pscore + 1;
+
+            }
+        } else {
             printf("Wrong Answer\n");
         }
+        printf("\n");
+    }
 
-            }
-            if(Cscore > Pscore ){
-                printf("Computer wins %d to %d\n",Cscore,Pscore);
-            }
-            else if(Cscore < Pscore ){
-                printf("Player wins %d to %d\n",Pscore,Cscore);
-            }
-           else if(Cscore = Pscore ){
-                printf("No winner it is a draw!\n");
-            }
+    if(Cscore > Pscore ){
+        printf("\nComputer wins %d to %d\n",Cscore,Pscore);
+    } else if(Cscore < Pscore ){
+        printf("\nPlayer wins %d to %d\n",Pscore,Cscore);
+    } else if(Cscore = Pscore ){
+        printf("\nNo winner it is a draw!\n");
+    }
 
-
-reload();
     return 0;
+}
+
+int main()
+{
+    reload();
+    int i;
+    srand(time(NULL));
+    printf("Rock=1 , Paper= 2, and Scissors= 3\n");
+
 }
 
